691a9053732f2ede42d866221a31b3a4aa13f5c8
Luciano Fiandesio <luciano@fiandes.io>
2016-03-02 16:58:51 +0100

    fixed bootstrap

--- app/index.js
+++ app/index.js
@@ -1,6 +1,13 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+require("./css/style.css");
+
 import App from './components/App.jsx';
+
+
 
 ReactDOM.render(
 	<App />, 
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+
+})
+
 module.exports = {
   entry:[
-    './src/index.js' 
+    './app/index.js' 
   ],
   output: {
-    path: __dirname,
-    filename: 'app/js/main.js'
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
   },
   module: {
     loaders: [
       {
          test: /\.jsx?$/,
+         include: __dirname + '/app',
          loader: 'babel',
          exclude: /node_modules/
-      }
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
+
 }
